764076ca151fe65603f9d212ba66070d25aa92c0
Felix Hsu <xuchenfei@gmail.com>
2012-10-21 22:01:01 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwLeave::Application.config.secret_token = 'a02aef3610cf8ef1a32beef03beaf489b9762a75699fd77509eab5a30f7777912310b9060ceec7e03a994755a3167a13dea7d082a5349caaa4f06bf464a991b1'
